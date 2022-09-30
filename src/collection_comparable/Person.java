0690567b7e8f42d9ea127334cc78054529fff184
admin <admin@DESKTOP-0QJ42PN>
2022-09-30 17:48:27 +0900

    Collection_Comparable

---
+++ src/collection_comparable/Person.java
@@ -0,0 +1,21 @@
+package collection_comparable;
+
+public class Person implements Comparable<Person> {
+
+	public String name;
+	public int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public int compareTo(Person o) {
+		//Person 자신의 age와 parameter의 age 값을 비교함
+		if(age < o.age) return-1; //오름차순
+		else if(age == o.age) return 0;
+		else return 1;
+	}
+	
+}
