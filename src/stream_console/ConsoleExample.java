e53f04413ebfb3d2e14bfb9ad01a79f8b8b2e51a
admin <admin@DESKTOP-0QJ42PN>
2022-10-04 12:25:29 +0900

    Stream_Console

---
+++ src/stream_console/ConsoleExample.java
@@ -0,0 +1,23 @@
+package stream_console;
+
+import java.io.Console;
+
+public class ConsoleExample {
+
+	public static void main(String[] args) {
+		
+		Console console = System.console();
+		
+		System.out.print("아이디: ");
+		String id = console.readLine();
+		
+		System.out.println("패스워드: ");
+		char[] charPass = console.readPassword();
+		String strPassword = new String(charPass); //char[] 배열을 문자열로 생성
+		
+		System.out.println("---------------------------");
+		System.out.println(id);
+		System.out.println(strPassword);
+		
+	}
+}
