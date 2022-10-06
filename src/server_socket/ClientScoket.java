3a961ed06fbcd0a531fb2df7bd92bce60eca9df6
(PJW0913) <(purumi97@gmail.com)>
2022-10-06 10:57:55 +0900

    Server_Socket

--- src/server_socket/ClientScoket.java
+++ src/server_socket/ClientScoket.java
@@ -10,10 +10,10 @@ public class ClientScoket {
 		try {
 			//SocketServer의 ip와 port 정보를 이용해 ServerSocket에 연결할 Socket 인스턴스를 생성
 			
-			Socket s = new Socket("localhost", 9999);
+			Socket s = new Socket("172.30.1.13", 5001);
 			
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-			dout.writeUTF("안녕하세요 Client에서 보내는 메세지 입니다.");
+			dout.writeUTF("안녕하세요 박준우 Client에서 보내는 메세지 입니다.");
 			dout.flush();
 			dout.close();
 			s.close();
