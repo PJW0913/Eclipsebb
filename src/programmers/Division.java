ed45944399d3df105512320669299803bf2cf4fd
PJW0913 <purumi97@gmail.com>
2022-10-25 16:36:14 +0900

    Division_int

--- src/programmers/Division.java
+++ src/programmers/Division.java
@@ -1,7 +1,10 @@
 package programmers;
 
 public class Division {
-	public int solution(int num1, int num2) {
+	public int solution1(int num1, int num2) {
         return (int) Math.floor((double)num1/num2*1000);
+    }
+	public int solution2(int num1, int num2) {
+        return (int)((double)num1/num2*1000);
     }
 }
