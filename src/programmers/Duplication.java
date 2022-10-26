078757dfaf599f2c554ae417f84fe83ce508d7c3
PJW0913 <purumi97@gmail.com>
2022-10-26 17:14:28 +0900

    Duplication

---
+++ src/programmers/Duplication.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Duplication {
+
+	public int solution(int[] array, int n) {
+		
+		int count = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == n) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
