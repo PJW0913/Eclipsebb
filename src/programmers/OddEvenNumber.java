f121b498d6c92907d6c835145a44a45358492f36
PJW0913 <purumi97@gmail.com>
2022-10-26 20:43:54 +0900

    OddEvenNumber

--- src/programmers/OddEvenNumber.java
+++ src/programmers/OddEvenNumber.java
@@ -2,4 +2,21 @@ package programmers;
 
 public class OddEvenNumber {
 
+	public int[] solution(int[] num_list) {
+        int OddCount = 0;
+        int EvenCount = 0;
+        int[] new_list = new int[2];
+        
+		for(int i = 0; i < num_list.length; i++) {
+			if(num_list[i]%2 == 0) {
+				EvenCount++;
+			}else if(num_list[i]%2 == 1){
+				OddCount++;
+			}
+		}
+		new_list[0] = EvenCount;
+		new_list[1] = OddCount;
+		
+		return new_list;
+    }
 }
