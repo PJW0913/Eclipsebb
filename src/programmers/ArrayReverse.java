e0b254a95e86c4ae0aa75cd3f8661fcd0f0b628f
PJW0913 <purumi97@gmail.com>
2022-10-27 16:27:28 +0900

    ArrayReverse

--- src/programmers/ArrayReverse.java
+++ src/programmers/ArrayReverse.java
@@ -10,11 +10,7 @@ public class ArrayReverse {
         		break;
         	}
         	reverse[i] = num_list[num_list.length-1-i];
-        }
-        for(int i = 0; i < num_list.length; i++) {
-        	System.out.print(reverse[i]);
-        }
-        
+        }    
         return reverse;
     }
 }
