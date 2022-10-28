9f6efff1e717e3f9f6a9ea6210037cb1c867c1c4
PJW0913 <purumi97@gmail.com>
2022-10-28 15:48:26 +0900

    CipherAdd

---
+++ src/programmers/CipherAdd.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class CipherAdd {
+
+	public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        int cipher = (int)Math.log10(n)+1;
+        
+        for(int i = 1; i < cipher; i++) {
+        	sum +=( n/(Math.pow(10, i)))%10;
+        }
+        
+        answer = sum + (n%10);
+        
+        return answer;
+    }
+	public int solution1(int n) {
+        int answer = 0;
+        while(n != 0){
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
