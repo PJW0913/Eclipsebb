684b4b91277244dc8e0cc1aa0d9cc86d04ab8bf7
PJW0913 <purumi97@gmail.com>
2022-10-30 02:09:36 +0900

    MultipleInArray_Contin

--- src/programmers/Example.java
+++ src/programmers/Example.java
@@ -4,8 +4,10 @@ public class Example {
 
 	public static void main(String[] args) {
 		
-		EvenNumberAdd division = new EvenNumberAdd();
-		System.out.println(division.solution(10));
+		int[] array = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+		
+		MultipleInArray answer = new MultipleInArray();
+		answer.solution(3, array);
 
 	}
 
---
+++ src/programmers/MultipleInArray.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class MultipleInArray {
+	
+	public int[] solution(int n, int[] numlist) {
+        int count = 0;
+        int[] answer = new int[count];
+        
+        for(int i = 0; i < numlist.length; i++) {
+        	if(numlist[i]%n == 0) {
+        		answer[count] = numlist[i];
+        		count++;
+        	}
+        }
+        for(int i = 0; i < answer.length; i++) {
+        	System.out.println(answer[i]);
+        }
+        
+        return answer;
+    }
+}
