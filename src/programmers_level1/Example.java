d2505dc439bb9f3ff3f5294c8e2db598b74c7b59
PJW0913 <purumi97@gmail.com>
2022-11-01 20:13:11 +0900

    HarshadNumber_Contin

---
+++ src/programmers_level1/Example.java
@@ -0,0 +1,15 @@
+package programmers_level1;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		NumReverseArray anwers = new NumReverseArray();
+		
+//		System.out.println(anwers.solution(12345));
+//		anwers.solution(999999);
+		System.out.println(Math.pow(3, 3));
+
+	}
+
+}
