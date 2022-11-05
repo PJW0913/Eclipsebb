6e97e9df8b6a52aa6e5f4b612eb4c1a5c8453982
PJW0913 <purumi97@gmail.com>
2022-11-05 20:11:50 +0900

    PhonenumberHide_Contin

---
+++ src/programmers_level1/PhonenumberHide.java
@@ -0,0 +1,10 @@
+package programmers_level1;
+
+public class PhonenumberHide {
+
+	public String solution(String phone_number) {
+        String answer = "";
+        
+        return answer;
+    }
+}
