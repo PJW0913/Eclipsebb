419988dc249186a8c0b46a1cf3b3ccfa46ed77ba
PJW0913 <purumi97@gmail.com>
2022-11-06 19:02:08 +0900

    WatermelonWatermelon

---
+++ src/programmers_level1/Watermelon.java
@@ -0,0 +1,17 @@
+package programmers_level1;
+
+public class Watermelon {
+
+	public String solution(int n) {
+        String answer = "";
+        
+        for(int i = 0; i < n; i++) {
+        	if(i%2 == 0) {
+        		answer += "¼ö";
+        	}else if(i%2 == 1) {
+        		answer += "¹Ú";
+        	}
+        }
+        return answer;
+    }
+}
